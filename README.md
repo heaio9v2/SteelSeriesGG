4319935389bf587fb7dd2fd69058d65ca3a50f8e
heaio9v2 <jeff@weyenbergfamily.com>
2025-08-09 19:12:29 -0500

    Add README and PowerShell script to manage SteelSeriesGG application in multiuser environments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SteelSeriesGG
-Workarounds to make SteelSeries GG work in a multiuser Windows environment
+Workarounds to make SteelSeries GG work in a multiuser Windows environment.
+
+# Background
+SteelSeries hardware is great hardware; however, the software has a lot to be desired. For example, the SteelSeries GG application that backends the advanced capabilities of the SteelSeries Apex Pro and SteelSeries Aerox 3 Wireless does not work on a multiuser Windows 11 OS. But let me be clear, the software "works" in that it installs and launches; however, it does not work because on its own it launches multiple copies of itself and its backend proceses for each user who logs in which results in the keyboard and/or mouse glitching badly. 
+
+So, with SteelSeries unable or unwilling to fix this issue I reported MANY months ago, this repo is intended to help mitigate the issues until SteelSeries can get their software development act together.
---
+++ Scripts/Start-SteelSeriesGG.ps1
@@ -0,0 +1,38 @@
+# Start-SteelSeriesGG.ps1
+# This script starts the SteelSeriesGG application and minimizes its client windows.
+# Disable the option in the SteelSeriesGG application to run SteelSeriesGG at startup and use this script instead to launch at startup or login.
+# You may want to remove the SteelSeriesGG shortcuts in the Windows start menu ss another precaution to stop multiple instances from running. 
+
+# Add necessary types for interop with Windows API
+# This allows us to manipulate window states such as minimizing.
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class Win32 {
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+}
+"@
+
+# Enumerate any existing SteelSeriesGG processes to avoid launching multiple instances
+$ggProcesses = Get-Process -Name "SteelSeriesGG" -ErrorAction SilentlyContinue
+if ($ggProcesses.count -eq 0) { 
+    # No existing process found, start a new instance and wait a bit for it to initialize
+    Start-Process 'C:\Program Files\SteelSeries\GG\SteelSeriesGG.exe' -ArgumentList '-dbEnv=production', '-dataPath="C:\ProgramData\SteelSeries\GG"' -WorkingDirectory 'C:\Program Files\SteelSeries\GG'
+
+    # SteelSeriesGG launches 6 StelSeriesGGClient processes so wait until they are all running
+    $clientProcesses = 0
+    while ($clientProcesses.Count -lt 6) {
+        $clientProcesses = Get-Process -Name "SteelSeriesGGClient" -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 1
+    }
+
+    # Minimize all running SteelSeriesGGClient windows
+    foreach ($clientProcess in $clientProcesses) {
+        $hwnd = $clientProcess.MainWindowHandle
+        if ($hwnd -ne 0) {
+            # Minimize the window (SW_MINIMIZE = 6)
+            [void][Win32]::ShowWindow($hwnd, 6)
+        }
+    }
+}
